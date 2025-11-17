3e4e054b955d6e51890bb379e95362714d0210eb
TranThong0908 <thongtran09082007@gmail.com>
2025-11-17 22:39:32 +0700

    Update hieu.c

--- hieu.c
+++ hieu.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 int main() {
-    int a,b,Hieu; 
+    int a,b; 
 	printf("Nhap 2 so a va b: "); scanf("%d", &a,&b);
-    Hieu = a - b;
-    printf("Hieu cua %d va %d la: %d\n",a,b,Hieu);
+    printf("Hieu cua %d va %d la: %d\n",a-b);
 }
