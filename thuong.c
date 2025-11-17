3ccf79c26f503c7fa00b20bde78b82bdcc356246
nvmanh2k7-it <nvmanh.2k7@gmail.com>
2025-11-17 22:22:56 +0700

    Update thuong.c

--- thuong.c
+++ thuong.c
@@ -1,1 +1,17 @@
+#include <bits/stdc++.h>
+
+int main() {
+    double a, b;
+    printf("Nhap 2 so: ");
+    scanf("%lf %lf", &a, &b);
+
+    if(b != 0) {
+        double thuong = a / b;
+        printf("Thuong cua %.2lf va %lf la: %lf\n", a, b, thuong);
+    } else {
+        printf("Loi: Khong the chia cho 0!\n");
+    }
+
+    return 0;
+}
 
